3728d989be695b0e8c0727d413287c1d81c5ea36
Matheus-Ruchinski <matheus.sperkoski01@gmail.com>
2024-10-02 21:04:58 -0300

    Refatoracao do codigo fonte

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-// Declaração de variáveis no escopo global
 let vitorias
 let derrotas 
 let nivel
 let saldoVitorias
 
-// Função main, quando chamada recebe como parâmetro as vitórias e derrotas de um jogador e calcula o saldo de vitorias
 function main(vitorias, derrotas){
     saldoVitorias = vitorias - derrotas
-    // A função main após o calculo, chama a função a qual classifica o seu rank passando como parâmetro o saldo de Vitorias
     rank(saldoVitorias)
 }
 
 function rank(saldoVitorias){
-    // Após a função ser chamada, ela irá verificar o rank do jagador através do parâmetro recebido e pela estrutura de decisão 
-    if (saldoVitorias <= 10){
+    if (saldoVitorias < 0) {
+        console.log("O sistema não aceita números negativos, tente novamente!")
+    } else if (saldoVitorias <= 10) {
         nivel = "Ferro"
-        // quando entrar em um dos if, será atribuido o rank na variável nivel
-    } else if ((saldoVitorias >= 11) && (saldoVitorias <= 20)){
+    } else if (saldoVitorias <= 20) {
         nivel = "Bronze"
-        } else if ((saldoVitorias >= 21) && (saldoVitorias <= 50)){
-            nivel = "Prata"
-            } else if ((saldoVitorias >= 51) && (saldoVitorias <= 80)){
-                nivel = "Ouro"
-                } else if ((saldoVitorias >= 81) && (saldoVitorias <= 90)){
-                    nivel = "Diamante"
-                    } else if((saldoVitorias >= 91) && (saldoVitorias <= 100)){
-                        nivel = "Lendário"
-                        } else if (saldoVitorias >= 101){
-                            nivel = "Imortal"
-                            }
-    // retornará o rank após classificado pela estrutura de decisão
+    } else if (saldoVitorias <= 50) {
+        nivel = "Prata"
+    } else if (saldoVitorias <= 80) {
+        nivel = "Ouro"
+    } else if (saldoVitorias <= 90) {
+        nivel = "Diamante"
+    } else if (saldoVitorias <= 100) {
+        nivel = "Lendário"
+    } else if (saldoVitorias > 101) {
+        nivel = "Imortal"
+    } else {
+        console.log("Nível não encontrado, tente novamente!")
+    }
     return(nivel)
 }
 
-// chamando a função main e passando os seus parâmetros
-main(150, 30)
+main(90, 80)
 
-console.log(`O herói tem saldo de ${saldoVitorias}, está no nível de ${nivel}`)
+if (nivel) {
+    console.log(`O herói tem saldo de ${saldoVitorias}, está no nível de ${nivel}`)
+}
